e65223e0c253ce0c8e45281f115c126cacba0546
Workflex Info <info@workflex-hr.com>
2024-09-15 17:39:30 +0200

    users list

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './components/views/login/login.component';
+import { UsersListComponent } from './components/views/users-list/users-list.component';
 
 export const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: '', redirectTo: '/login', pathMatch: 'full' }, // Default redirect to login
+  { path: 'users-list', component: UsersListComponent },
 ];
